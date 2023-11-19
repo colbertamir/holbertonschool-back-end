fbc3437842204155beb7c2352b40ac3149b0cd27
colbertamir <colbertamir1516@gmail.com>
2023-11-19 11:48:08 -0600

    Changed imports

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Gather data from an API"""
 
-import requests
+import json
 import sys
+import urllib.error
+import urllib.request
+
 
 if __name__ == "__main__":
     """Check for the correct number of command-line arguments and their format"""
@@ -14,19 +17,17 @@ if __name__ == "__main__":
     employee_id = int(sys.argv[1])
 
     """URLs for fetching user & to-do data based on the employee ID"""
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
 
     try:
         """Fetch user data from API"""
-        response = requests.get(url)
-        response.raise_for_status()
-        user_data = response.json()
+        with urllib.request.urlopen(user_url) as response:
+            user_data = json.loads(response.read().decode())
 
         """Grabs to-do list for the user"""
-        response = requests.get(todo_url)
-        response.raise_for_status()
-        todos = response.json()
+        with urllib.request.urlopen(todo_url) as response:
+            todos = json.loads(response.read().decode())
 
         """Extract finished tasks & calculate total & finalized tasks count"""
         completed_tasks = [task['title'] for task in todos if task['completed']]
@@ -38,6 +39,6 @@ if __name__ == "__main__":
         for task in completed_tasks:
             print(f"\t{task}")
 
-    except requests.exceptions.RequestException as e:
+    except urllib.error.URLError as e:
         """Handle request-related exceptions"""
         print(f"Error occurred: {e}")
